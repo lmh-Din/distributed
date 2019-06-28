4f3eeb08925469ff5bac9a37a5b4db257508c74a
Wazi <2863547436@qq.com>
2019-06-28 16:22:18 +0800

    test

--- seata/src/main/java/com/distributed/seata/SeataApplication.java
+++ seata/src/main/java/com/distributed/seata/SeataApplication.java
@@ -1,20 +1,28 @@
 package com.distributed.seata;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class,
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class,
 		DataSourceTransactionManagerAutoConfiguration.class,
 		JdbcTemplateAutoConfiguration.class})
-@EnableEurekaClient
 @Slf4j
 public class SeataApplication implements CommandLineRunner {
+
+	@Autowired
+	private DataSource storageDataSource;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SeataApplication.class, args);
@@ -23,5 +31,15 @@ public class SeataApplication implements CommandLineRunner {
 	@Override
 	public void run(String... strings) throws Exception {
 		log.info("seata is running.....");
+		storageQuery();
 	}
+
+	private void storageQuery() throws SQLException {
+		Connection connection = storageDataSource.getConnection();
+		PreparedStatement statement = connection.prepareStatement("SELECT * FROM storage_tbl");
+		ResultSet resultSet = statement.executeQuery();
+		log.info("resultSet:[{}]", resultSet.toString());
+		statement.close();
+	}
+
 }
